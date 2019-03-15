1c0dc0f7eeae5f9f13b89625f65f3fbc4cdc19e4
Emir Mekić <emir.mekic@live.com>
2019-03-15 23:24:21 +0100

    Updates

---
+++ precache-manifest.43b7e682e96cd8a03fd6d41665f33dd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6dea580d01e901b4bc4",
+    "url": "/twitter-clone/static/js/runtime~main.d3dd3cd8.js"
+  },
+  {
+    "revision": "b41b788d08b63c2acc8b",
+    "url": "/twitter-clone/static/js/main.a87916b9.chunk.js"
+  },
+  {
+    "revision": "027d6a12eb35c579067d",
+    "url": "/twitter-clone/static/js/2.13c4ec98.chunk.js"
+  },
+  {
+    "revision": "b41b788d08b63c2acc8b",
+    "url": "/twitter-clone/static/css/main.bcc901e7.chunk.css"
+  },
+  {
+    "revision": "ea88f2bee500cc6a64e378be80bd59cb",
+    "url": "/twitter-clone/index.html"
+  }
+];
