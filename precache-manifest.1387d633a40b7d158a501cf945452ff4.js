d57b42ba426dd536b4ac0a21628c70f7507c4b42
Emir Mekić <emir.mekic@live.com>
2019-04-09 01:10:35 +0200

    Updates

---
+++ precache-manifest.1387d633a40b7d158a501cf945452ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1d25a20a3bf62cccd66",
+    "url": "/MOP-TwitterClone/static/js/runtime~main.e346ef8c.js"
+  },
+  {
+    "revision": "b16327109343efc1b521",
+    "url": "/MOP-TwitterClone/static/js/main.ef6133ae.chunk.js"
+  },
+  {
+    "revision": "f7428459480107164051",
+    "url": "/MOP-TwitterClone/static/js/2.683e4c5b.chunk.js"
+  },
+  {
+    "revision": "b16327109343efc1b521",
+    "url": "/MOP-TwitterClone/static/css/main.5b991ff2.chunk.css"
+  },
+  {
+    "revision": "259e38af5fd537db15c1e52fe9b96ef8",
+    "url": "/MOP-TwitterClone/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MOP-TwitterClone/precache-manifest.e8a41d4bcadec8974c55a5ebeceb4501.js"
+  "/MOP-TwitterClone/precache-manifest.1387d633a40b7d158a501cf945452ff4.js"
 );
 
 workbox.clientsClaim();
